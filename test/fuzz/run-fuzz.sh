dea4b12c83e69e565ff49d1007e5f11dd9189a1b
Elias Kosunen <elias.kosunen@gmail.com>
2022-03-03 17:35:30 +0200

    Fix errors #7

--- test/fuzz/run-fuzz.sh
+++ test/fuzz/run-fuzz.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 self=$0
-fuzzer=$1
+repo_dir=$1
+fuzzer=$2
 
 if [[ $fuzzer = "roundtrip" ]]; then
   data="int"
@@ -11,11 +12,10 @@ else
   data=$fuzzer
 fi
 
-repo_dir=$(git rev-parse --show-toplevel)
 data_dir=$repo_dir/test/fuzz
 script_dir=$(dirname "$(readlink -f "$self")")
 
 mkdir "$script_dir/corpus-$fuzzer"
 "$script_dir/fuzz-$fuzzer" -dict="$data_dir/dict-$data.txt" \
   "$script_dir/corpus-$fuzzer" "$data_dir/seed-corpora/$data" \
-  "${@:2}"
+  "${@:3}"
