d8021612539957cf99fc174c80f64c7061efc767
baptistapedro <pedrojorgesbaptista@gmail.com>
2022-12-04 02:46:32 +0100

    Update and rename fuzz.cpp to fuzz_scan.cpp

--- fuzzers/fuzz_scan.cpp
+++ fuzzers/fuzz_scan.cpp
@@ -1,4 +1,3 @@
-
 #include <scn/scn.h>
 #include <string>
 #include <stdio.h>
@@ -6,14 +5,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <iostream>
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     std::string word;
     std::string buf(reinterpret_cast<const char *>(data), size);
     buf.push_back('\0');
     auto result = scn::scan(buf, "{}", word);
-
-    std::cout << word << '\n';                      // Will output "Hello"
-    std::cout << result.range_as_string() << '\n';  // Will output " world!"
+    
+    // std::cout can damage the fuzz perfomance
+    // but considering the library and methods it is fuzzing seems reasaonable
+    std::cout << word << '\n';                      
+    std::cout << result.range_as_string() << '\n';  
     return 0;
 }
